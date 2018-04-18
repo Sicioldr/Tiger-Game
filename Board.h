0964a3a972b48238a8ae96c2ba00fbc1152d068f
Morrak <luke.barron@grandecom.net>
2018-04-17 20:29:35 -0500

    Update Board.h

--- Board.h
+++ Board.h
@@ -1,20 +1,34 @@
 #ifndef BOARD_H_INCLUDED
 #define BOARD_H_INCLUDED
 
+#include <cmath>
+#include <iostream>
+
 #include "Token.h"
-#include "Node.h"
+
+using namespace std;
 
 const int GAME_SIZE = 90;
 const int HUNTER_COUNT = 18;
+const int LAIR_ROOT = 3;
+const int FIELD_ROOT = 9;
+
+enum Direction{None, Up, Down, Left, Right, UpLeft, DownLeft, UpRight, DownRight};
 
 class Board{
 private:
     Tiger shereKhan;
     Hunter jaeger[HUNTER_COUNT];
     Node spots[GAME_SIZE];
+    int hunterPopulation;
+
+    void arrangeNodes();
 
 public:
+    Board();
 
+    //Check adjacencies
+    Direction checkAdjacent(int origin, int target);
 };
 
 #endif // BOARD_H_INCLUDED
