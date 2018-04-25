95d6a97607f6310ed01b198a15ca3c62f27c50cd
Morrak <luke.barron@grandecom.net>
2018-04-25 14:10:38 -0500

    Update Node.h

--- Node.h
+++ Node.h
@@ -4,15 +4,19 @@
 class Node{
 private:
     bool occupied;
+    int identity;
+    ///As below, these are hopefully going away
     int relativeX, relativeY; //Set w/in containing Board
 
 public:
     //Overridden constructor
     Node();
 
-    //Switch for occupation
+    //Setters for statuses
     void setOccupied(bool status);
+    void setIdentity(int nomDeGuerre);
 
+    ///Trying to ditch this system; the functions don't need 2D locations to work
     //Set coordinates; used in Board construction
     void setX(int x); //Col
     void setY(int y); //Row
